a0c13b363e0b6487ae6b665957ed91027fc67ca2
saikat <saikatmondal410@gmail.com>
2019-02-06 20:44:21 +0530

    bring the day counter js code to new file in new folder js/myjs/day_counter.js

---
+++ js/myjs/day_counter.js
@@ -0,0 +1,14 @@
+document.getElementById("days-in-kgp").setAttribute("data-to",formatAMPM());
+function formatAMPM() {
+					    var d = new Date(),
+					    minutes = d.getMinutes().toString().length == 1 ? '0'+d.getMinutes() : d.getMinutes(),
+					    hours = d.getHours().toString().length == 1 ? '0'+d.getHours() : d.getHours(),
+						ampm = d.getHours() >= 12 ? 'pm' : 'am',
+					    months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'],
+					    days = ['Sun','Mon','Tue','Wed','Thu','Fri','Sat'];
+						var date2 = new Date("07/24/2017");//mm/dd/yyyy
+						var timeDiff = Math.abs(date2.getTime() - d.getTime());
+						var diffDays = Math.ceil(timeDiff / (1000 * 3600 * 24)); 
+						return diffDays;
+						}
+							
